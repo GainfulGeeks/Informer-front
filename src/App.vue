304a27e1a2852c74c6f489ee226705a52b98ed9d
atefeh <hemmati.atefeh97@gmail.com>
2023-12-11 20:56:57 +0330

    use fake data to create employee table

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,14 @@
 
 <template>
       <Sidebar />
+  <main class="font-sans bg-secondary">
+    <div class="flex-1 overflow-x-hidden overflow-y-auto p-4 m-5 md:ml-64">
+      <h2 class="text-2xl font-semibold mb-4">Page Title?!</h2>
+      <RouterView />
 
-
-  <RouterView />
+    </div>
+  </main>
+  
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,8 @@
 </script>
 
 <template>
-  <main class="font-sans bg-secondary">
-
     <router-link to="/employees" class="flex justify-center items-center">  employee list </router-link>
 
-  </main>
 </template>
 
 <style>
