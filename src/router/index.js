a26ede89341336b8d635fd4bb5dd94a244435faf
Ashkan Taravati <ashkan.taravati@gmail.com>
2024-01-12 14:38:18 +0330

    rename LoginPage component

--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/Login.vue')
+      component: () => import('../views/LoginPage.vue')
     }
   ]
 })
