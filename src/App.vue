63d6a86306eae25dc0aa70e7091f2a764375881a
Ashkan Taravati <ashkan.taravati@gmail.com>
2024-01-12 14:36:03 +0330

    separate layout for authenticated pages from non-authenticated login

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup>
-  import Sidebar from "@/components/Sidebar.vue";
 </script>
 
 <template>
-      <Sidebar />
-  <main class="font-sans bg-secondary">
-    <div class="flex-1 overflow-x-hidden overflow-y-auto p-4 m-5 md:ml-64">
-      <h2 class="text-2xl font-semibold mb-4">Page Title?!</h2>
-      <RouterView />
-
-    </div>
-  </main>
+  <RouterView />
   
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AuthenticatedLayout from "../components/AuthenticatedLayout.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
+      path: '/app',
+      name: '',
+      component: AuthenticatedLayout,
+      children: [
+        {
+          path: '/',
+          name: 'dashboard',
+          component: HomeView
+        },
+        {
+          path: '/about',
+          name: 'about',
+          component: () => import('../views/AboutView.vue')
+        },
 
-    {
-      path: '/employees',
-      name: 'employees',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/EmployeeList.vue')
+        {
+          path: '/employees',
+          name: 'employees',
+          component: () => import('../views/EmployeeList.vue')
+        }
+      ]
     },
 
     {
       path: '/login',
       name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/Login.vue')
     }
   ]
