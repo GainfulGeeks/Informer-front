b5790f693a92d78d0a999e8ae6f40d8c6ad539ed
Ashkan Taravati <ashkan.taravati@gmail.com>
2024-01-12 15:22:19 +0330

    install vue-types and add IInput

---
+++ src/components/kit/IInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <label :for="id" class="block text-sm font-medium text-gray-500">{{label}}</label>
+    <input
+      :placeholder="placeholder"
+      type="text"
+      :id="id"
+      name="email"
+      class="mt-1 p-2 w-full border-b-2 border-gray-300 focus:border-b-primary focus:outline-none transition-colors duration-300"
+    />
+  </div>
+</template>
+
+<script setup>
+import { string, number, object, oneOfType, bool } from 'vue-types'
+ defineProps({
+  id: string().def(undefined),
+  disabled: bool(),
+  modelValue: oneOfType([string(), number(), object()]).def('').isRequired,
+  label: string(),
+  placeholder:string()
+});
+
+</script>
+
+<style lang="scss" scoped></style>
