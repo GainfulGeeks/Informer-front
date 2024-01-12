1dab0278d34255d8a7f48c1564f612b47de173e2
Ashkan Taravati <ashkan.taravati@gmail.com>
2024-01-12 15:53:48 +0330

    update to vue 3.4

--- src/components/kit/IInput.vue
+++ src/components/kit/IInput.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <label :for="id" class="block text-sm font-medium text-gray-500">{{label}}</label>
+    <label :for="id" class="block text-sm font-medium text-gray-500">{{ label }}</label>
     <input
-      :placeholder="placeholder"
+      v-model="modelValue"
+      :placeholder
       type="text"
-      :id="id"
+      :id
       name="email"
       class="mt-1 p-2 w-full border-b-2 border-gray-300 focus:border-b-primary focus:outline-none transition-colors duration-300"
     />
@@ -12,15 +13,16 @@
 </template>
 
 <script setup>
-import { string, number, object, oneOfType, bool } from 'vue-types'
- defineProps({
+import { string, bool } from 'vue-types'
+import { defineModel } from 'vue'
+defineProps({
   id: string().def(undefined),
   disabled: bool(),
-  modelValue: oneOfType([string(), number(), object()]).def('').isRequired,
   label: string(),
-  placeholder:string()
-});
+  placeholder: string()
+})
 
+const modelValue = defineModel()
 </script>
 
 <style lang="scss" scoped></style>
