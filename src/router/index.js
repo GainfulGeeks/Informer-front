0ec2de3b365b8958ecf53f795835d50271ccd026
atefeh <hemmati.atefeh97@gmail.com>
2024-02-09 18:20:56 +0330

    If the route requires authentication and user is not authenticated, redirect to login

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,54 @@
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+// import AuthenticatedLayout from "../components/AuthenticatedLayout.vue";
+
+
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/app',
+//       name: '',
+//       component: AuthenticatedLayout,
+//       children: [
+//         {
+//           path: '/',
+//           name: 'dashboard',
+//           component: HomeView
+//         },
+//         {
+//           path: '/about',
+//           name: 'about',
+//           component: () => import('../views/AboutView.vue')
+//         },
+
+//         {
+//           path: '/employees',
+//           name: 'employees',
+//           component: () => import('../views/EmployeeList.vue')
+//         }
+//       ]
+//     },
+
+//     {
+//       path: '/login',
+//       name: 'login',
+//       component: () => import('../views/LoginPage.vue')
+//     }
+//   ]
+// })
+
+// export default router
+
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AuthenticatedLayout from "../components/AuthenticatedLayout.vue";
+import LoginPage from '../views/LoginPage.vue'
 
+function isAuthenticated() {
+  //  authentication logic should be  here
+  return localStorage.getItem('token') !== null;
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,15 +74,25 @@ const router = createRouter({
           name: 'employees',
           component: () => import('../views/EmployeeList.vue')
         }
-      ]
+      ],
+      meta: { requiresAuth: true }
     },
 
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginPage.vue')
+      component: LoginPage
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  // If the route requires authentication and user is not authenticated, redirect to login
+  if (to.matched.some(record => record.meta.requiresAuth) && !isAuthenticated()) {
+    next('/login');
+  } else {
+    next();
+  }
 })
 
 export default router
