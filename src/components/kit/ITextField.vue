d04a3ea9b9b3b5aebcbe160f037647c7417772c7
babak <babak.behzad79@gmail.com>
2024-03-18 08:04:33 -0700

    feat: added type property to textfield component

--- src/components/kit/ITextField.vue
+++ src/components/kit/ITextField.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
     <label :for="id" class="block text-sm font-medium text-gray-500">{{ label }}</label>
-    <input
-        v-model="modelValue"
-        :placeholder="placeholder"
-        :id="id"
-        :name="id"
-        v-on="onEvents"
-        class="mt-1 p-2 w-full border-b-2 border-gray-300 focus:border-b-primary focus:outline-none transition-colors duration-300"
-      /> 
-      <div v-if="errorMessage" class="text-red-600">{{ errorMessage }}</div>
+    <input :type="type" v-model="modelValue" :placeholder="placeholder" :id="id" :name="id" v-on="onEvents"
+      class="mt-1 p-2 w-full border-b-2 border-gray-300 focus:border-b-primary focus:outline-none transition-colors duration-300" />
+    <div v-if="errorMessage" class="text-red-600">{{ errorMessage }}</div>
   </div>
 </template>
 
@@ -19,6 +13,7 @@ import { useField } from 'vee-validate';
 import { string } from 'vue-types'
 
 const props = defineProps({
+  type: string().def('text'),
   id: string(),
   label: string(),
   placeholder: string(),
@@ -32,7 +27,7 @@ const emit = defineEmits([
 const modelValue = defineModel()
 const name = toRef(props, 'rule')
 
-const {errorMessage, handleChange, handleBlur} = useField(name , undefined)
+const { errorMessage, handleChange, handleBlur } = useField(name, undefined)
 const onEvents = {
   focus: e => {
     handleBlur(e)
